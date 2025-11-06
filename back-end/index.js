ad49d2160cf605296118b3feeb79130d312c7a49
SKINPALL <89147001+SK1NPALL@users.noreply.github.com>
2025-11-07 01:47:36 +0700

    Update connect with mySQL
    -Create new query and table in mySQL for about me information
    -Import mysql2 and setup
    -Import nodemon for run backend server
    -Import dotenv for hide the important information

--- back-end/index.js
+++ back-end/index.js
@@ -1,11 +1,55 @@
+require('dotenv').config()
 const express = require('express')
+const mysql = require('mysql2/promise')
 const app = express()
-const port = 8000
+const port = Number(process.env.PORT)
 
 app.use(express.json())
 
-app.listen(port,(req,res)=>{
+let conn = null;
 
-    console.log('Server start at '+port)
+const initMySQL = async () => {
+
+    conn = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE_1
+
+    })
+
+}
+
+app.get('/info' , async (req ,res) =>{
+
+    try{
+
+        const result = await conn.query('SELECT * FROM aboutme')
+
+
+        res.json({
+
+            message : 'Get info complete',
+            info : result[0]
+
+        })
+
+    }catch(error) {
+
+        console.log(error.message)
+        res.status(500).json({
+            
+            message : 'Something went wrong.'
+
+        })
+
+    }
+
+})
+
+app.listen(port, (req, res) => {
+
+    initMySQL()
+    console.log('Server start at ' + port)
 
 })
