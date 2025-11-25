670c8267f59b36736dd8c20fac1b694d5ef315e8
SKINPALL <89147001+SK1NPALL@users.noreply.github.com>
2025-11-26 01:20:44 +0700

    Update database

--- back-end/index.js
+++ back-end/index.js
@@ -16,7 +16,8 @@ const initMySQL = async () => {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE_1
+        database: process.env.DB_DATABASE_1,
+        port : 3306
 
     })
 
